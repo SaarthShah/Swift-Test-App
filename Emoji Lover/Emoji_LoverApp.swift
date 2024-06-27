87656a066239bf5ea66de40afd7b9bb14fb83fe7
Saarth Shah <69341449+SaarthShah@users.noreply.github.com>
2024-06-27 12:35:03 -0700

    Initial Commit

---
+++ Emoji Lover/Emoji_LoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_LoverApp.swift
+//  Emoji Lover
+//
+//  Created by Saarth Shah on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_LoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
